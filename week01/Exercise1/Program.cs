9450aeb9c0dfe8d28ea69dc7797f35ceb1520792
brownmputu <brownmputu5@gmail.com>
2025-09-09 20:43:25 +0100

    input and output exercice

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,17 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("a cofee,thee,rise,fufu,bean,fish,cheeken,duck,beef");
+        Console.WriteLine("what is your first name?");
+        string firstName = Console.ReadLine();
 
-        Console.WriteLine("what are you gonna cook this morning?");
-        string morning = Console.ReadLine();
+        Console.WriteLine("what is your last name?");
+        string  lastName = Console.ReadLine();
 
-        Console.WriteLine("what are you gonna cook this afterNoon?");
-        string afterNoon = Console.ReadLine();
-
-        Console.WriteLine("what are you gonna cook this evening?");
-        string evening = Console.ReadLine();
-
-        Console.WriteLine($"today you will eat :{morning} and {afterNoon} and {evening}");
+        Console.WriteLine($"your name is:{lastName} ,{firstName} {lastName}");
     }
 }
